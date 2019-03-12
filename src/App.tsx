093188720242238e8c45b4b3afff422c0463c09d
pskibicki <skibicki.pawel@outlook.com>
2019-03-12 13:12:49 +0100

    disable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as cheerio from 'cheerio';
 import * as React from 'react';
-import * as request from 'request';
+// import * as request from 'request';
 
 interface IState {
   messages: string[];
@@ -21,12 +21,38 @@ class App extends React.Component<{}, IState> {
   }
 
   public async componentDidMount() {
-    this.checkForNewAwardsRequest();
-    setInterval(this.processResponse, 3 * 1000);
+    // await this.checkForNewAwardsRequest();
+    // setInterval(this.checkForNewAwardsRequest, 3 * 1000);
   }
 
   public async checkForNewAwardsRequest() {
-    return request('https://polakpotrafi.pl/projekt/auto-stop-race-2019/', this.processResponse);
+    // return await request('https://polakpotrafi.pl/projekt/auto-stop-race-2019/', this.processResponse);
+    return fetch('https://polakpotrafi.pl/projekt/auto-stop-race-2019/', {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'plain/text'
+      }
+    }).then((response: any) => {
+      const newMessages = this.state.messages;
+
+      const $ = cheerio.load(response.body);
+      const selectors = ['.card.clearfix'];
+      selectors.forEach(selector => {
+        if ($(selector).length > CURR_ELEMENT_COUNT) {
+          newMessages.push('NEW AWARD!!!');
+        } else {
+          newMessages.push(':(');
+        }
+      });
+
+      this.setState({ messages: newMessages });
+    }).catch((err: any) => {
+      const newMessages = this.state.messages;
+      newMessages.push('Błąd!');
+      this.setState({ messages: newMessages });
+    });
   }
 
   public processResponse(err: any, resp: any, html: any) {
@@ -34,9 +60,9 @@ class App extends React.Component<{}, IState> {
     if (!err) {
       const $ = cheerio.load(html);
       const selectors = ['.card.clearfix'];
-      selectors.forEach(selector => {        
+      selectors.forEach(selector => {
         if ($(selector).length > CURR_ELEMENT_COUNT) {
-          newMessages.push('NEW AWARD!!!');          
+          newMessages.push('NEW AWARD!!!');
         } else {
           newMessages.push(':(');
         }
@@ -50,15 +76,14 @@ class App extends React.Component<{}, IState> {
   public render() {
     const { messages } = this.state;
     return (
-      <div>
+      <React.Fragment>
         {messages.map((message: string, i: any) => {
           return (
             <div key={i}>
               <p>{message}</p>
-              <br/>
             </div>);
         })}
-      </div>
+      </React.Fragment>
     );
   }
 }
