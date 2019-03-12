39a1f71267ea383f01489a997fec77e23a4b31d7
pskibicki <skibicki.pawel@outlook.com>
2019-03-12 11:33:36 +0100

    polaq v1

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import * as cheerio from 'cheerio';
+import * as React from 'react';
+import * as request from 'request';
+
+interface IState {
+  messages: string[];
+}
+
+const CURR_ELEMENT_COUNT = 7;
+
+class App extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      messages: [] as string[],
+    };
+
+    this.processResponse = this.processResponse.bind(this);
+    this.checkForNewAwardsRequest = this.checkForNewAwardsRequest.bind(this);
+  }
+
+  public async componentDidMount() {
+    this.checkForNewAwardsRequest();
+    setInterval(this.processResponse, 3 * 1000);
+  }
+
+  public async checkForNewAwardsRequest() {
+    return request('https://polakpotrafi.pl/projekt/auto-stop-race-2019/', this.processResponse);
+  }
+
+  public processResponse(err: any, resp: any, html: any) {
+    const newMessages = this.state.messages;
+    if (!err) {
+      const $ = cheerio.load(html);
+      const selectors = ['.card.clearfix'];
+      selectors.forEach(selector => {        
+        if ($(selector).length > CURR_ELEMENT_COUNT) {
+          newMessages.push('NEW AWARD!!!');          
+        } else {
+          newMessages.push(':(');
+        }
+      });
+    } else {
+      newMessages.push('Błąd!');
+    }
+    this.setState({ messages: newMessages });
+  }
+
+  public render() {
+    const { messages } = this.state;
+    return (
+      <div>
+        {messages.map((message: string, i: any) => {
+          return (
+            <div key={i}>
+              <p>{message}</p>
+              <br/>
+            </div>);
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
